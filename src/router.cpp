d413ff05da2d772adba38696c7a56834d0bce109
Andrew Peterson <16apeterson@gmail.com>
2022-07-07 01:22:08 -0500

    chore: increase depth

--- src/router.cpp
+++ src/router.cpp
@@ -98,7 +98,7 @@ void Net::Router::handleRoutes(httplib::Server& server) {
             Minimax::SuggestedMove moveTest = paranoid.minimax(
                 grid, 
                 state, 
-                2, 
+                4, 
                 true,
                 std::numeric_limits<float>::lowest(),
                 std::numeric_limits<float>::max(),
