5ba43a5626187eaa13a30b40cb9866168a6bce0d
Andrew Peterson <16apeterson@gmail.com>
2022-07-07 09:53:09 -0500

    chore: debug

--- run.bash
+++ run.bash
@@ -1,5 +1,6 @@
 echo This can take some time, there are over 30000 lines of code to compile in the libraries...
 # clang++-7 -pthread -std=c++17 -o main main.cpp minimax.cpp
 # ./main
+git pull
 make
 ./bin/main
