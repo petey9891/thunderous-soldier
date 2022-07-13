a7b042c20493fb3d599197021b43cc825279f4a4
Andrew Peterson <16apeterson@gmail.com>
2022-07-12 21:12:31 -0500

    chore: added logging file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# thunderous-soldier
-A minimax based battlesnake
+# Thunderous Soldier
+
+Thunderous Soldier is a paranoid [minimax](https://en.wikipedia.org/wiki/Minimax) based [Battlesnake](https://play.battlesnake.com/) making use of [alpha-beta pruning](https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning) and [flood fill](https://en.wikipedia.org/wiki/Flood_fill) algorithms to make predictions about the future state of the game. The snake evaluates all possible moves by ourselves and the enemy.
---
+++ include/logging.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "json.hpp"
+#include "battlesnake.hpp"
+
+enum LogLevel {
+    INFO,
+    DEBUG
+};
+
+#define CURRENT_LOG_LEVEL INFO
+
+std::string inline levelToSring(LogLevel level) {
+    switch (level) {
+        case INFO: 
+            return "INFO";
+        case DEBUG:
+            return "DEBUG";
+        default:
+            return "INFO"; 
+    }
+}
+
+bool inline validateLog(LogLevel level) {
+    if (level > CURRENT_LOG_LEVEL) {
+        return false;
+    }
+    return true;
+}
+
+void inline LOG(LogLevel level, std::string message, bool extraSpacer = false) {
+    if (validateLog(level)) {
+        if (extraSpacer) std::cout << std::endl;
+        std::cout << "[" << levelToSring(level) << "] " << message << std::endl;
+    }
+}
+
+void inline LOG(LogLevel level, std::string message, Battlesnake::Point point, bool extraSpacer = false) {
+    if (validateLog(level)) {
+        if (extraSpacer) std::cout << std::endl;
+        std::cout << "[" << levelToSring(level) << "] " << message << point;
+    }
+}
+
+void inline LOG(LogLevel level, std::string message, Battlesnake::Direction direction, bool extraSpacer = false) {
+    if (validateLog(level)) {
+        if (extraSpacer) std::cout << std::endl;
+        std::cout << "[" << levelToSring(level) << "] " << message << direction << std::endl;
+    }
+}
+
+void inline LOG(LogLevel level, std::string message, nlohmann::json data, bool extraSpacer = false) {
+    if (validateLog(level)) {
+        if (extraSpacer) std::cout << std::endl;
+        std::cout << "[" << levelToSring(level) << "] " << message << data << std::endl;
+    }
+}
+
+void inline LOG(LogLevel level, std::string message, int value, bool extraSpacer = false) {
+    if (validateLog(level)) {
+        if (extraSpacer) std::cout << std::endl;
+        std::cout << "[" << levelToSring(level) << "] " << message << value << std::endl;
+    }
+}
+
+void inline LOG(LogLevel level, std::string message, float value, bool extraSpacer = false) {
+    if (validateLog(level)) {
+        if (extraSpacer) std::cout << std::endl;
+        std::cout << "[" << levelToSring(level) << "] " << message << value << std::endl;
+    }
+}
